0bd928563c10894328426d4bb647f926acce470d
Aaarl24 <154050515+Aaarl24@users.noreply.github.com>
2025-02-07 00:39:11 +0800

    Add files via upload

---
+++ APP DEV/js.js
@@ -0,0 +1,29 @@
+function dName() {
+    document.getElementById("name").innerHTML="Arl John A. Austria";
+}
+
+function dDream() {
+    document.getElementById("dream").innerHTML="GAME DEVELOPER";
+}
+
+function DispContact() {
+    document.getElementById("contacts").innerHTML = `
+        <div class="contact">
+            <div class="contact-info">09085009426</div>
+            <div class="contact-info">arl.john.austria@adamson.edu.ph</div>
+            <div class="contact-info">Lian, Batangas 4216</div>
+        </div>
+    `;
+}
+
+
+function DispInfo() {
+    document.getElementById("info").innerHTML="Arl John Austria is nineteen years old, enthusiastic about games, and very interested in coding skills. He is a Cancer sign born into an intuitive nature and creativity. He is approachable and good for casual conversation and deep subjects as well. A great portion of his free time is spent on video games where he loves the thrills and the challenges within them. Besides being interested in games, he is devoted to programming and aims to make his passion for technology into considerable expertise. His favorite dish is curry, which he enjoys because of its complex flavors and soothing warmth. Green is his favorite color, reflecting his fresh outlook on life and his affinity with nature. He would appreciate the balance between the moment and self-improvement, always looking to grow while appreciating what's happening. He'd be motivated by overcoming the obstacle and exploring the infinite possibilities that coding has to offer. It's whether he is perfecting a new game or getting over a programming hurdle, or just hanging out with friends, Arl will always approach his endeavors with a curiosity and composed manner.";
+}
+
+function DispSkills() {
+    document.getElementById("skill1").innerHTML="Teamwork and Collaboration";
+    document.getElementById("skill2").innerHTML="Organization Skills";
+    document.getElementById("skill3").innerHTML="Computer Skills";
+    document.getElementById("skill4").innerHTML="Active listening";
+}
